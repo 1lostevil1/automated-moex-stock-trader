7e2dd6fe8f9b8d27850d893e300b84d066b9a83f
1lostevil1 <ki-04@yandex.ru>
2025-06-15 16:43:25 +0300

    initial test commit

---
+++ automated-moex-stock-trader/DataLoader/src/main/java/org/example/MarketDataPipeline/StreamService.java
@@ -0,0 +1,37 @@
+package org.example.MarketDataPipeline;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.tinkoff.piapi.contract.v1.MarketDataResponse;
+import ru.tinkoff.piapi.core.stream.MarketDataStreamService;
+import ru.tinkoff.piapi.core.stream.MarketDataSubscriptionService;
+import ru.tinkoff.piapi.core.stream.StreamProcessor;
+
+import java.util.function.Consumer;
+
+@Service
+public class StreamService {
+
+    private final MarketDataStreamService marketDataStreamService;
+    private final StreamProcessor<MarketDataResponse> processor;
+    private final Consumer<Throwable> errorHandler;
+
+    @Autowired
+    public StreamService(MarketDataStreamService marketDataStreamService, StreamProcessor<MarketDataResponse> processor, Consumer<Throwable> errorHandler) {
+        this.marketDataStreamService = marketDataStreamService;
+        this.processor = processor;
+        this.errorHandler = errorHandler;
+    }
+
+    @PostConstruct
+    public void subscribeOrderBook() {
+        MarketDataSubscriptionService subscription = marketDataStreamService.newStream(
+                "e6123145-9665-43e0-8413-cd61b8aa9b13",
+                processor,
+                errorHandler
+        );
+
+        //subscription.subscribeOrderbook(List.of("e6123145-9665-43e0-8413-cd61b8aa9b13"), 10);
+    }
+}
