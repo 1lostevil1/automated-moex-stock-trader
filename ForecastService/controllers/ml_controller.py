8da775bdaf97e0f036db8c4b70c2b1171f1bd8ac
leonid <ulyanov.leo.v@gmail.com>
2025-06-23 02:52:55 +0300

    added controller

---
+++ ForecastService/controllers/ml_controller.py
@@ -0,0 +1,41 @@
+﻿from ..scripts.model import StockPredictionModel
+from ..scripts.db_connect import DbConnect
+import os
+import pandas as pd
+
+DEFAULT_MODELS_DIR = "../models"
+
+
+# TODO: сделать переобучение моделей параллельно
+# TODO: написать API для контроллера
+class MLController:
+    def __init__(self, models_dir: str = DEFAULT_MODELS_DIR):
+        self.models: dict[str, StockPredictionModel] = {}
+        self.__dbConnect = DbConnect()
+        self.__models_dir = models_dir
+        self.__init_models()
+
+    def __init_models(self):
+        all_items = os.listdir(self.__models_dir)
+        folders = [item for item in all_items if os.path.isdir(os.path.join(self.__models_dir, item))]
+        tickers = self.__dbConnect.get_tickers()
+        for ticker in tickers:
+            if ticker not in folders:
+                self.models[ticker] = StockPredictionModel()
+                train_data: pd.DataFrame = self.__dbConnect.get_data(ticker)
+                self.models[ticker].init_model(ticker, train_data, self.__models_dir)
+            else:
+                path_to_model = os.path.join(self.__models_dir, ticker)
+                self.models[ticker].load_model(path_to_model)
+
+    def retrain_models(self) -> None:
+        all_items = os.listdir(self.__models_dir)
+        folders = [item for item in all_items if os.path.isdir(os.path.join(self.__models_dir, item))]
+        tickers = self.__dbConnect.get_tickers()
+        for ticker in tickers:
+            new_data: "pd.DataFrame" = self.__dbConnect.get_data(ticker)
+            if ticker not in folders:
+                self.models[ticker] = StockPredictionModel()
+                self.models[ticker].init_model(ticker, new_data, self.__models_dir)
+            else:
+                self.models[ticker].retrain_model(ticker, new_data, self.__models_dir)
