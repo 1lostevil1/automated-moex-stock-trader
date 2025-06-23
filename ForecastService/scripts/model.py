5983418fc00ddcebdebca1e26ea21171a4e8fc3b
leonid <ulyanov.leo.v@gmail.com>
2025-06-23 17:22:09 +0300

    feat: added threading, plot builder

--- ForecastService/scripts/model.py
+++ ForecastService/scripts/model.py
@@ -1,39 +1,55 @@
 ﻿import pandas as pd
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import LSTM, Dense
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard, CSVLogger
 import os
 from sklearn.preprocessing import MinMaxScaler
-from data_processor import preprocess_data
+from scripts.data_processor import preprocess_data, NUM_OF_FEATS
 
-DEFAULT_LOGS_DIR = "../logs"
+import matplotlib.pyplot as plt  # TODO: потом удалить
+
+DEFAULT_LOGS_DIR: str = f"..\\logs"
 
 
 def create_model(X_train) -> "Sequential":
     model = Sequential()
-    model.add(LSTM(100, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+    model.add(
+        LSTM(100, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])))
     model.add(LSTM(100, return_sequences=False))
-    model.add(Dense(25))
+    model.add(Dense(50))
     model.add(Dense(1))
 
     return model
 
 
 class StockPredictionModel:
-    def __init__(self):
+    def __init__(self, output_features: int = NUM_OF_FEATS):
         self.__model_curr = Sequential()
         self.__model_prev = Sequential()
+        self.__scaler_X = [MinMaxScaler(feature_range=(0, 1)) for _ in range(output_features)]
+        self.__scaler_Y = MinMaxScaler(feature_range=(0, 1))
         self.model_in_use = self.__model_curr
+        self.output_features = output_features
 
     def init_model(self, ticker: str, df: "pd.DataFrame", dir_path: str):
-        scaler = MinMaxScaler(feature_range=(0, 1))
         training_data_len = int(len(df) * 0.8)
         model_path = os.path.join(dir_path, ticker)
-        X_train, y_train, _, _ = preprocess_data(df, scaler, training_data_len)
+        X_train, y_train, X_test, y_test = preprocess_data(df, self.__scaler_X, self.__scaler_Y,
+                                                           training_data_len)
+
         self.__model_curr = self.train_model(X_train, y_train, ticker, model_path)
         self.model_in_use, self.__model_prev = self.__model_curr, self.__model_curr
         self.__model_curr.save(f'{model_path}/{ticker}_prev.keras')
+
+        # TODO: эту часть потом удалить
+        predictions = self.model_in_use.predict(X_test)
+        predictions = self.__scaler_Y.inverse_transform(predictions)
+        plt.plot(df['time'].iloc[training_data_len:], predictions, '--', color='red', label='Predicted open price')
+        plt.plot(df['time'].iloc[training_data_len:], y_test, color='blue', label='Real open price')
+        plt.legend()
+        plt.show()
 
     def load_model(self, dir_path: str) -> None:
         self.__model_curr = tf.keras.models.load_model(dir_path)
@@ -42,7 +58,11 @@ class StockPredictionModel:
     def train_model(self, X_train, y_train, ticker: str, model_path: str,
                     log_path: str = DEFAULT_LOGS_DIR) -> "Sequential":
         model = create_model(X_train)
-        model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+        r2_score = tf.keras.metrics.R2Score()
+        model.compile(optimizer='adam', loss='mean_squared_error',
+                      metrics=['mean_squared_error',
+                               'mean_absolute_error',
+                               r2_score])
 
         model_checkpoint = ModelCheckpoint(f'{model_path}/{ticker}_curr.keras',
                                            save_best_only=True,
@@ -56,23 +76,34 @@ class StockPredictionModel:
             csv_logger
         ]
 
-        model.fit(
-            X_train, y_train,
-            batch_size=32,
-            epochs=100,
-            callbacks=callbacks_list
-        )
+        with tf.device('/GPU:0'):
+            model.fit(
+                X_train, y_train,
+                batch_size=32,
+                epochs=300, # TODO: если будет оверфиттинг, вернуть обратно 100 эпох, либо добавить EralyStopping
+                callbacks=callbacks_list
+            )
 
         return model
 
-    def get_prediction(self, model_path: str):
-        pass
+    def get_prediction(self, input: list[float]) -> float:
+        input_scaled = [scaler.transform(input[:, i:i + 1]) for i, scaler in enumerate(self.__scaler_X)]
+        input_scaled = np.concatenate(input_scaled, axis=1)
+
+        X_test = []
+        X_test.append(input_scaled)
+        X_test = np.array(X_test)
+        X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], X_test.shape[2]))
+
+        prediction = self.model_in_use.predict(X_test)
+        prediction = self.__scaler_Y.inverse_transform(prediction)[0][0]
+
+        return prediction
 
     def retrain_model(self, ticker: str, df: "pd.DataFrame", dir_path: str) -> None:
-        scaler = MinMaxScaler(feature_range=(0, 1))
         training_data_len = int(len(df) * 0.8)
         model_path = os.path.join(dir_path, ticker)
-        X_train, y_train, _, _ = preprocess_data(df, scaler, training_data_len)
+        X_train, y_train, _, _ = preprocess_data(df, self.__scaler_X, self.__scaler_Y, training_data_len)
         self.model_in_use = self.__model_prev
         self.__model_curr = self.train_model(X_train, y_train, ticker, model_path)
         self.model_in_use, self.__model_prev = self.__model_curr, self.__model_curr
